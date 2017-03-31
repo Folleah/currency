c921f07f136740ebd75c8e03d13e26f977f551e0
Folleah5 <folleah5@gmail.com>
2017-03-31 08:39:22 +0400

    Privated methods

--- src/ConverterLogic.php
+++ src/ConverterLogic.php
@@ -4,19 +4,21 @@ namespace Folleah\Currency;
 
 class ConverterLogic
 {
-    protected static function roundTo($val)
+    private static function roundTo($val)
     {
         return (int)(round($val, 4) * 10000);
     }
 
-    protected static function roundFrom($val)
+    private static function roundFrom($val)
     {
         return round($val / 10000, 2);
     }
 
     public static function convert($currentCurrencyRelation, $currencyRelation, $value) {
-        $val = self::roundTo($currencyRelation) * self::roundTo($value)
-        / self::roundTo($currentCurrencyRelation);
+        $val = 
+            self::roundTo($currencyRelation) 
+            * self::roundTo($value)
+            / self::roundTo($currentCurrencyRelation);
 
         return self::roundFrom($val);
     }
